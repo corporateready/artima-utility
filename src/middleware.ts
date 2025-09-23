7bb63fe7967bdc932e2bb30e5196ce57b21f4a4b
Evgheni <86593030+nev-front-dev@users.noreply.github.com>
2025-09-23 15:49:42 +0300

    added clerk auth update

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,37 +14,3 @@ export const config = {
     "/(api|trpc)(.*)",
   ],
 };
-
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-// import { NextResponse } from "next/server";
-
-// const isPublicRoute = createRouteMatcher(["/", "/navigatePage(.*)"]);
-// const isAuthRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
-
-// export default clerkMiddleware(async (auth, req) => {
-//   const { userId } = await auth();
-//   const { pathname } = req.nextUrl;
-
-//   // Если пользователь авторизован и находится на странице авторизации
-//   if (userId && isAuthRoute(req)) {
-//     return NextResponse.redirect(new URL("/src/app/navigatePage", req.url));
-//   }
-
-//   // Если пользователь не авторизован и пытается попасть на защищенную страницу
-//   if (!userId && !isPublicRoute(req) && !isAuthRoute(req)) {
-//     return NextResponse.redirect(new URL("/", req.url));
-//   }
-
-//   // Защищаем приватные маршруты
-//   if (!isPublicRoute(req)) {
-//     await auth.protect();
-//   }
-// });
-
-// export const config = {
-//   matcher: [
-//     "/((?!.+\\.[\\w]+$|_next).*)",
-//     "/",
-//     "/(api|trpc)(.*)",
-//   ],
-// };
